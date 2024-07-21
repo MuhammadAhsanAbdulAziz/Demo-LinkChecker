b3b893c80e0788bf8b3cd295e295587afbaf8059
muhammadahsanabdulaziz <maahsan36@gmail.com>
2024-07-21 15:14:13 +0500

    first commit

---
+++ app/src/main/java/com/example/demolinkchecker/MyBroadcastReceiver.kt
@@ -0,0 +1,11 @@
+package com.example.demolinkchecker
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
